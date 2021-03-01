0d60b9f43f19ac08c1eff675d4bb3710e8c8a724
jekim21 <jekim@uchicago.edu>
2021-03-01 05:47:51 +0000

    first git changes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
 
   # CREATE
   get("/insert_user_record", {:controller => "users", :action => "create" })
+  get("/user_sign_up", { :controller => "users", :action => "new_registration_form"})
+  get("/user_sign_out", { :controller => "users", :action => "toast_cookies"})
+  get("/user_sign_in", { :controller => "users", :action => "new_session_form"})
+  
+  post("/verify_credentials", { :controller => "users", :action => "authenticate"})
+  
 
   # READ
   get("/users", {:controller => "users", :action => "index"})
