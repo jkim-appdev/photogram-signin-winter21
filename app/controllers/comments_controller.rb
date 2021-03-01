e7638cf0e85999cd9e21dda070f320371879da70
jekim21 <jekim@uchicago.edu>
2021-03-01 06:03:27 +0000

    new changes to photo id

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,21 @@
 class CommentsController < ApplicationController
+  def index
+    comments = Comment.all.order({ :created_at => :asc})
 
+    render({ :json => comments.as_json})
+  end
+
+  def show
+    the_id = params.fetch( :the_comment_id)
+    comment = Comment.where({ :id => the_id}).at(0)
+
+    render({ :json => comment.as_json})
+  end
 
   def create
     comment = Comment.new
 
-    comment.author_id = params.fetch("input_author_id")
+    comment.author_id = session.fetch( :user_id)
     comment.photo_id = params.fetch("input_photo_id")
     comment.body = params.fetch("input_body")
 
